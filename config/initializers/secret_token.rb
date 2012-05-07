aa0eaf6fa3ec3e0380224c7d1b47c10428d8d9e0
wouterharkema <wouter.harkema@gmail.com>
2012-05-07 12:01:25 +0200

    initial commit 2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wolter::Application.config.secret_token = '2192e607db6e0810a283d217f44d87af312436860cb23bd763f81207b8dcc46a8a5aa988be710b5bbc34bb3380251ede31b58ecc2f3927a229c11f2f5c247b36'
